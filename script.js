c8e276e7729a13dae2cb02194e593fd351aade71
Claudio Meira <claudiomeirabs@gmail.com>
2024-01-17 22:12:15 -0300

    requisitos do 1 ao 13 realizados! Sorry o commit bomba!

---
+++ script.js
@@ -0,0 +1,12 @@
+const validLogin = document.getElementById('login');
+const validEmail = document.getElementById('email');
+const validPassword = document.getElementById('password');
+
+validLogin.addEventListener('click', (event) => {
+  event.preventDefault();
+  const email = validEmail.value;
+  const password = validPassword.value;
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else alert('Email ou senha inválidos.');
+});
